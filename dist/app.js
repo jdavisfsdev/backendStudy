ce24917b63b52b8097d7ba7b7ebf70169cb28f4c
jddadev <joethebartenderpdx@gmail.com>
2022-01-23 12:33:20 -0800

    Brought in some express types, still messing around with primitive types

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Practicing with types
-Object.defineProperty(exports, "__esModule", { value: true });
-const greeting = 'hello';
-// Type array of numbers
-const numbers = [1, 2, 3];
