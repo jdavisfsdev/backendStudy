52cb74dc6fe06d0072f99b843b3b075162452e60
jddadev <joethebartenderpdx@gmail.com>
2022-01-23 13:09:54 -0800

    jest installed

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const add = (a, b) => a + b;
+app.get('/', (req, res) => {
+    console.log(add(5, 4));
+    res.send('Hello');
+});
+app.listen(5000, () => console.log('Server running'));
